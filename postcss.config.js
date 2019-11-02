943e6e5f9803482c371a47f021a4c50970fc4c35
Pierre Mdawar <p.mdawar@gmail.com>
2019-11-02 19:44:43 +0200

    Added PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,14 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue'
+  ]
+});
+
 module.exports = {
   plugins: [
-    require('autoprefixer')
+    require('autoprefixer'),
+    // Use PurgeCSS in production only
+    ...process.env.NODE_ENV === 'production' ? [purgecss] : []
   ]
 };
